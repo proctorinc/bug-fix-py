24555e874624334beae5ba763f8d14e6922a3e25
mproctor-scw <92598535+mproctor-scw@users.noreply.github.com>
2022-11-14 15:49:55 -0800

    Update setup.py
    updated setup.py package name

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 
-setup(name='py-bug-fix',
+setup(name='bugfixpy',
       version='1.0.0',
       description='SCW automated bug fix program',
       author='Matt Proctor',
       author_email='matthewalanproctor@gmail.com',
-    #   url='',
-      packages=['py-bug-fix'],
+      packages=['bugfixpy'],
       license="MIT",
       install_requires=[
         'GitPython==3.1.27',
