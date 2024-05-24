b562732a6214e77c9693ca6298b00b2a446e8c35
Matt Proctor <mproctor@securecodewarrior.com>
2024-05-24 11:24:39 -0700

    Fixed duplicate scraping issue

--- bugfixpy/modes/transition_mode.py
+++ bugfixpy/modes/transition_mode.py
@@ -13,7 +13,7 @@ class TransitionMode(RunnableMode, ScraperMode):
         super().__init__(self.MODE, test_mode=False)
 
     def run(self) -> None:
-        self.scrape_challenge_data()
+        challenge_data = self.scrape_challenge_data()
         challenge_request_issue = prompt_user.get_challenge_request_issue()
 
         is_bulk_transition_required = prompt_user.if_bulk_transition_is_required()
@@ -22,7 +22,7 @@ class TransitionMode(RunnableMode, ScraperMode):
 
         TransitionIssues(
             FixResult([fix_message], is_bulk_transition_required, False),
-            self.scrape_challenge_data(),
+            challenge_data,
             challenge_request_issue,
         ).run()
 
